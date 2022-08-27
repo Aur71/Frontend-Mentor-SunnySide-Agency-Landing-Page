cb7efc79d7f6157f1a115ce8488b906d4219db38
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-08-27 12:31:27 +0300

    Again

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const toggleBtn = document.querySelector(".toggleBtn");
-const ul = document.querySelector("ul");
+// const toggleBtn = document.querySelector(".toggleBtn");
+// const ul = document.querySelector("ul");
 
-toggleBtn.addEventListener("click", () => {
-    ul.classList.toggle("active")
-})
+// toggleBtn.addEventListener("click", () => {
+//     ul.classList.toggle("active")
+// })
